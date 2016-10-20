f3f27223c3f1610b6d0b8ea5cdf65794aeffac31
root <ppkrauss@gmail.com>
2016-10-20 21:21:20 +0000

    opts

--- scripts/testmail.php
+++ scripts/testmail.php
@@ -1,11 +1,18 @@
-<?php 
+<?php
     ini_set( 'display_errors', 1 );
     error_reporting( E_ALL );
-    $from = "peter@yellowtown.org";
-    $to = isset($argv[1])? "dino.orchi@gmail.com": "ppkrauss@gmail.com";
+
+    $opt = ['d'=>'', 'f'=>'', 'h'=>true];
+    $opt = array_merge( $opt, getopt("d:f:h") );
+
+    if (!$opt['h'])
+	die("\nAJUDA, options disponiveis:\n -f fromName (dft ppkrauss) -d domainName (dft yellow)\n");
+    $from = 'test@'.( $opt['d']? $opt['d'] : 'yellowtown.org' );
+    $to   = $opt['f']? $opt['f']: 'ppkrauss@gmail.com';
+
     $subject = "ola mamae!";
     $message = "Este e o email da mamae.\n Nao pode cair na caixa de spam... \n Etc. e tal isso eh um email normal";
     $headers = "From:" . $from;
     mail($to,$subject,$message, $headers);
-    echo "\nTest email sent to $to!\n";
+    echo "\nTest email from $from sent to $to!\n";
 ?>
