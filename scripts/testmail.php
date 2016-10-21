13fdf77ef78aba37212025f270d1675b11344a1a
Peter <ppkrauss@gmail.com>
2016-10-21 16:19:38 -0200

    add opt -s

--- scripts/testmail.php
+++ scripts/testmail.php
@@ -6,11 +6,11 @@
     $opt = array_merge( $opt, getopt("d:t:h") );
 
     if (!$opt['h'])
-	die("\nAJUDA, options disponiveis:\n -t toName (dft ppkrauss) -d fromDomainName (dft yellow)\n");
+	die("\nAJUDA, options disponiveis:\n\t -t toName (dft ppkrauss)\n\t-d fromDomainName (dft yellow)\n\t-s extra subject\n");
     $from = 'test@'.( $opt['d']? $opt['d'] : 'yellowtown.org' );
     $to   = $opt['t']? $opt['t']: 'ppkrauss@gmail.com';
 
-    $subject = "ola mamae!";
+    $subject = "ola mamae! ".$opt['s'];
     $message = "Este e o email da mamae.\n Nao pode cair na caixa de spam... \n Etc. e tal isso eh um email normal";
     $headers = "From:" . $from;
     mail($to,$subject,$message, $headers);
