dd8f85043db96cd982b41a3409d50ec0c710a666
Peter <ppkrauss@gmail.com>
2016-11-04 11:29:43 -0200

    add cats e rev

--- scripts/step4_checks.sh
+++ scripts/step4_checks.sh
@@ -1,12 +1,20 @@
 ## fabio edita aqui e depois da git pull la
 
 echo '--- VERSOES ---'
-
-mysql --version
 postconf -d | grep mail_version
 dovecot --version
 
 echo '--- SERVICES ---'
+service postfix status    | grep postfix.service
+service dovecot status    | grep dovecot.service
+service --status-all
 
-service postfix status
-service dovecot status
+
+echo '-- -- -- /etc/postfix/*.cf'
+cat /etc/postfix/mysql-virtual-mailbox-domains.cf
+cat /etc/postfix/mysql-virtual-mailbox-maps.cf
+cat /etc/postfix/mysql-virtual-alias-maps.cf
+cat /etc/postfix/mysql-virtual-email2email.cf
+cat /etc/postfix/master.cf
+
+
