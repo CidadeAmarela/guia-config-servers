480dde12735458e3685f4d17af0089cf6462bbf4
Fabio Dino Orchi <dino.orchi@gmail.com>
2016-11-04 11:26:14 -0200

    Update step4_checks.sh
    verifica versões e serviços do postfix e dovecot.

--- scripts/step4_checks.sh
+++ scripts/step4_checks.sh
@@ -1,1 +1,12 @@
 ## fabio edita aqui e depois da git pull la
+
+echo '--- VERSOES ---'
+
+mysql --version
+postconf -d | grep mail_version
+dovecot --version
+
+echo '--- SERVICES ---'
+
+service postfix status
+service dovecot status
