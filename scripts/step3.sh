6f84fc2f2e1ef0275c488c58508cda59e87780a1
Peter <ppkrauss@gmail.com>
2017-05-18 22:22:56 -0300

    Update step3.sh

--- scripts/step3.sh
+++ scripts/step3.sh
@@ -6,20 +6,19 @@ fi
 apt update
 
 #Apache2
-apt install apache2 libapache2-mod-php7.1
+apt install apache2 libapache2-mod-php7.0
 
 #Fornecer permissão /www
 chown -R www-data:www-data /var/www
 chmod -R g+rw /var/www
 
-#PHP7.1
-apt install php7.1 php7.1-mysql php7.1-mbstring php7.1-imap php7.1-mcrypt
+#PHP7
+apt install php7.0 php7.0-mysql php7.0-mbstring php7.0-imap php7.0-mcrypt
 
 #MySQL
 apt install mysql-server
 
 #postgreSQL
-apt install postgresql-9.6 postgresql-contrib-9.6 postgis-2.3
+apt install postgresql-9.5 postgresql-contrib-9.5 postgis-2.2
 
 echo '.. próximo passo PostFix'
-
