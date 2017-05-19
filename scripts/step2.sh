a6fc5848594de519bb92babbf484ffcaff675e05
Peter <ppkrauss@gmail.com>
2017-05-18 22:11:15 -0300

    Update step2.sh

--- scripts/step2.sh
+++ scripts/step2.sh
@@ -1,6 +1,4 @@
 echo '--- SEGUIR O GUIA para criar usuários ---'
-## ... falta criar usuário e logar com usuário
-# adduser newUserName
-# exit
-# ssh newUserName@server
+echo ' ... falta criar usuário e logar com usuário'
+prinrf " adduser newUserName\n exit\n ssh newUserName@server"
 echo '--- ... depois logar como usuário ---'
