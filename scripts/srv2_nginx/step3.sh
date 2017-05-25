82b8652b55d61fa1f71a5a60c5e550e11637e4e0
Peter <ppkrauss@gmail.com>
2017-05-25 20:15:00 -0300

    Update step3.sh

--- scripts/srv2_nginx/step3.sh
+++ scripts/srv2_nginx/step3.sh
@@ -15,14 +15,15 @@ chmod -R g+rw /var/www
 #PHP7
 apt install php7.0 php7.0-mysql php7.0-mbstring php7.0-imap php7.0-mcrypt
 
-#Python (default??)
-apt install python
+#Python3 (default??)
+apt install python3
 
 
 #MySQL
 apt install mysql-server
 
 #postgreSQL .. The v9.6 have many problems in UBUNTU 16 LST 
-apt install postgresql-9.5 postgresql-contrib-9.5 postgis-2.2
+# apt install postgresql-9.5 postgresql-contrib-9.5 postgis-2.2
+apt install postgresql postgresql-contrib  postgis
 
 echo '.. próximo passo PostFix'
