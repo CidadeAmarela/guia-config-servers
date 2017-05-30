5615753b4cf40eb94c070009feb8ef3535788d4e
Peter <ppkrauss@gmail.com>
2017-05-30 08:31:21 -0300

    ops

--- scripts/srv2_nginx/step3_checks.sh
+++ scripts/srv2_nginx/step3_checks.sh
@@ -2,14 +2,18 @@ echo '--- VERSOES ---'
 
 psql --version
 php --version
+python --version
+python3 --version
 git --version
+
+sudo ufw app list
 
 echo '--- ETC ---'
 php --info | grep -v REQUEST_TIME
 
 echo '--- SERVICES ---'
 
-service apache2 status    | grep -E "apache2.service|Drop-In"
+service nginx status    | grep -E -i "nginx.service|Drop-In"
 service mysql status      | grep mysql.service
 service postgresql status | grep postgresql.service
 service --status-all
